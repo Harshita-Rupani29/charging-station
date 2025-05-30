f32251532f217a430f41acd60d3c5d2d04a1c7e9
Harshita <harshitarupani4@gmail.com>
2025-05-30 10:46:53 +0530

    model for charging station

---
+++ backend/models/ChargingStation.js
@@ -0,0 +1,64 @@
+const chargingStationSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    location: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        default: "Point",
+      },
+      coordinates: {
+        type: [Number], // [longitude, latitude]
+        required: true,
+      },
+      address: {
+        type: String,
+        required: true,
+      },
+    },
+    powerOutput: {
+      type: Number, // in kW
+      required: true,
+      min: 0,
+    },
+    // connectorType: {
+    //   type: String,
+    //   required: true,
+    //   enum: ["Type1", "Type2", "CCS", "CHAdeMO", "Tesla", "Other"],
+    // },
+    connectorType: {
+      type: String,
+      required: true,
+      enum: [
+        "Type1",
+        "Type2",
+        "CCS1",
+        "CCS2",
+        "CHAdeMO",
+        "Tesla",
+        "NACS",
+        "GBT-AC",
+        "GBT-DC",
+        "Other",
+      ],
+    },
+
+    status: {
+      type: String,
+      enum: ["active", "inactive"],
+      default: "active",
+      lowercase: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+chargingStationSchema.index({ location: "2dsphere" });
+
+module.exports = mongoose.model("ChargingStation", chargingStationSchema);
